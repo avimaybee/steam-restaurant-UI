25d9606877672546108a7a34351dc7614f293fab
avi <avimaybe7@gmail.com>
2025-12-06 18:32:42 +0530

    Remove deprecated serverActions config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   trailingSlash: true,
   images: {
     unoptimized: true,
